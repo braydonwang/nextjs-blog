e79e17f09f8a65fb1023e2d795dc400a7547a6ff
Braydon Wang <braydon.wang@gmail.com>
2022-05-01 14:05:14 -0400

    changed date-fns

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
-import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -36,7 +35,7 @@ export default function Home({ allPostsData }) {
               </Link>
               <br />
               <small className={utilStyles.lightText}>
-                <Date dateString={date} />
+                {date}
               </small>
             </li>
           ))}
